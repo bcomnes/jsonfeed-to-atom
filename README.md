92b4f0da033d64cd4fc3fd17662cf1dc95a53469
Bret Comnes <bcomnes@gmail.com>
2018-04-14 14:16:52 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,120 @@
-# jsonfeed-to-atom
-Convert a JSON feed to an atom feed
+# jsonfeed-to-atom [![stability][0]][1]
+[![npm version][2]][3] [![build status][4]][5]
+[![downloads][8]][9] [![js-standard-style][10]][11]
+
+Convert a JSON feed to an atom feed.
+
+## Installation
+```console
+$ npm install jsonfeed-to-atom
+```
+
+## Usage
+
+```js
+const jsonfeedToAtom = require('./jsonfeed-to-atom')
+const someJSONFeed = require('./load-some-json-feed-data.json')
+
+const atomFeed = jsonfeedToAtom(someJSONFeed) // Returns an atom formatted json feed
+```
+
+Example input:
+
+```json
+{
+ "version": "https://jsonfeed.org/version/1",
+ "title": "bret.io log",
+ "home_page_url": "https://bret.io",
+ "feed_url": "https://bret.io/feed.json",
+ "description": "A running log of announcements, projects and accomplishments.",
+ "next_url": "https://bret.io/2017.json",
+ "icon": "https://bret.io/icon-512x512.png",
+ "author": {
+  "name": "Bret Comnes",
+  "url": "https://bret.io",
+  "avatar": "https://gravatar.com/avatar/8d8b82740cb7ca994449cccd1dfdef5f?size=512"
+ },
+ "items": [
+  {
+   "date_published": "2018-04-07T20:48:02.000Z",
+   "content_text": "Wee wooo this is some content. \n Maybe a new paragraph too",
+   "url": "https://bret.io/my-text-post",
+   "id": "https://bret.io/my-text-post-2018-04-07T20:48:02.000Z"
+  },
+  {
+   "date_published": "2018-04-07T22:06:43.000Z",
+   "content_html": "<p>Hello, world!</p>",
+   "title": "This is a blog title",
+   "url": "https://bret.io/my-blog-post",
+   "external_url": "https://example.com/some-external-link",
+   "id": "https://bret.io/my-blog-post-2018-04-07T22:06:43.000Z"
+  }
+ ]
+}
+```
+
+Example output:
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>bret.io log</title>
+  <id>https://bret.io/feed.xml</id>
+  <updated>2018-04-07T22:06:43.000Z</updated>
+  <link rel="self" type="application/atom+xml" href="https://bret.io/feed.xml"/>
+  <link rel="alternate" type="application/json" href="https://bret.io/feed.json"/>
+  <link rel="alternate" type="text/html" href="https://bret.io"/>
+  <link rel="next" href="https://bret.io/2017.xml"/>
+  <author>
+    <name>Bret Comnes</name>
+    <uri>https://bret.io</uri>
+  </author>
+  <generator uri="https://github.com/bcomnes/jsonfeed-to-atom#readme" version="1.0.0">jsonfeed-to-atom</generator>
+  <rights>© 2018 Bret Comnes</rights>
+  <subtitle>A running log of announcements, projects and accomplishments.</subtitle>
+  <entry>
+    <id>https://bret.io/my-text-post-2018-04-07T20:48:02.000Z</id>
+    <title>Wee wooo this is some content.</title>
+    <updated>2018-04-07T20:48:02.000Z</updated>
+    <published>2018-04-07T20:48:02.000Z</published>
+    <content type="text">Wee wooo this is some content. 
+ Maybe a new paragraph too</content>
+    <link rel="alternate" href="https://bret.io/my-text-post"/>
+  </entry>
+  <entry>
+    <id>https://bret.io/my-blog-post-2018-04-07T22:06:43.000Z</id>
+    <title>This is a blog title</title>
+    <updated>2018-04-07T22:06:43.000Z</updated>
+    <published>2018-04-07T22:06:43.000Z</published>
+    <content type="html">
+      <![CDATA[<p>Hello, world!</p>]]>
+    </content>
+    <link rel="alternate" href="https://bret.io/my-blog-post"/>
+    <link rel="related" href="https://example.com/some-external-link"/>
+  </entry>
+</feed>
+```
+
+## API
+### `jsonfeedToAtom(parsedJsonfeed)`
+Coverts a parsed JSON feed into an atom feed.  Returns the string of the atom feed.
+
+## See also
+
+- [JSON Feed: Mapping RSS and Atom to JSON Feed](https://jsonfeed.org/mappingrssandatom)
+- [AtomEnabled: Developers > Syndication](https://web.archive.org/web/20160113103647/http://atomenabled.org/developers/syndication/#link)
+- [bcomnes/generate-feed](https://github.com/bcomnes/generate-feed)
+
+## License
+[MIT](https://tldrlegal.com/license/mit-license)
+
+[0]: https://img.shields.io/badge/stability-experimental-orange.svg?style=flat-square
+[1]: https://nodejs.org/api/documentation.html#documentation_stability_index
+[2]: https://img.shields.io/npm/v/jsonfeed-to-atom.svg?style=flat-square
+[3]: https://npmjs.org/package/jsonfeed-to-atom
+[4]: https://img.shields.io/travis/bcomnes/jsonfeed-to-atom/master.svg?style=flat-square
+[5]: https://travis-ci.org/bcomnes/jsonfeed-to-atom
+[8]: http://img.shields.io/npm/dm/jsonfeed-to-atom.svg?style=flat-square
+[9]: https://npmjs.org/package/jsonfeed-to-atom
+[10]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
+[11]: https://github.com/feross/standard
---
+++ index.js
@@ -0,0 +1,148 @@
+const builder = require('xmlbuilder')
+const packageInfo = require('./package.json')
+const striptags = require('striptags')
+const trimRight = require('trim-right')
+const trimLeft = require('trim-left')
+
+function jsonfeedToAtomObject (jf) {
+  const { title, version, feed_url: feedURL } = jf
+  if (!title) throw new Error('jsonfeed-to-atom: missing title')
+  if (version !== 'https://jsonfeed.org/version/1') throw new Error('jsonfeed-to-atom: JSON feed version 1 required')
+  if (!feedURL) throw new Error('jsonfeed-to-atom: missing feed_url')
+  const atomFeedURL = feedURL.replace('json', 'xml')
+  const now = new Date()
+  const atom = {
+    feed: { // Required items
+      '@xmlns': 'http://www.w3.org/2005/Atom',
+      title,
+      id: atomFeedURL,
+      updated: now.toISOString()
+    }
+  }
+
+  atom.feed.link = [
+    { '@rel': 'self', '@type': 'application/atom+xml', '@href': atomFeedURL },
+    { '@rel': 'alternate', '@type': 'application/json', '@href': feedURL }
+  ]
+  if (jf.home_page_url) {
+    atom.feed.link.push({'@rel': 'alternate', '@type': 'text/html', '@href': jf.home_page_url})
+  }
+  if (jf.next_url) {
+    atom.feed.link.push({'@rel': 'next', '@href': jf.next_url.replace('json', 'xml')})
+  }
+
+  if (jf.author) {
+    atom.feed.author = {}
+    if (jf.author.name) atom.feed.author.name = jf.author.name
+    if (jf.author.url) atom.feed.author.uri = jf.author.url
+  }
+  atom.feed.generator = {
+    '@uri': packageInfo.homepage,
+    '@version': packageInfo.version,
+    '#text': packageInfo.name
+  }
+  if (jf.favicon || jf.icon) atom.feed.icon = jf.favicon || jf.icon
+  if (jf.author && jf.author.name) atom.feed.rights = `© ${now.getFullYear()} ${jf.author.name}`
+  if (jf.description) atom.feed.subtitle = jf.description
+
+  if (jf.items && jf.items.length > 0) {
+    let mostRecentlyUpdated = '0'
+    atom.feed.entry = []
+    jf.items.forEach(item => {
+      const entry = { // Required fields
+        id: item.id,
+        title: generateRequiredTitle(item),
+        updated: item.date_modified || item.date_published || now.toISOString()
+      }
+      if (item.date_published) entry.published = item.date_published
+      if (item.date_published && (item.date_published > mostRecentlyUpdated)) mostRecentlyUpdated = item.date_published
+      if (item.date_modified && (item.date_modified > mostRecentlyUpdated)) mostRecentlyUpdated = item.date_modified
+
+      if (item.author) {
+        entry.author = {}
+        if (item.author.name) entry.author.name = item.name
+        if (item.author.url) entry.author.uri = item.url
+      }
+
+      entry.content = []
+
+      if (item.content_html) {
+        const htmlContent = {
+          '@type': 'html',
+          '#cdata': item.content_html
+        }
+        entry.content.push(htmlContent)
+      }
+
+      if (item.content_text) {
+        const textContent = {
+          '@type': 'text',
+          '#text': item.content_text
+        }
+        entry.content.push(textContent)
+      }
+
+      entry.link = []
+      if (item.url) {
+        entry.link.push({
+          '@rel': 'alternate',
+          '@href': item.url
+        })
+      }
+
+      if (item.external_url) {
+        entry.link.push({
+          '@rel': 'related',
+          '@href': item.external_url
+        })
+      }
+
+      if (item.attachments) {
+        item.attachments.forEach(attachment => {
+          const enclosure = { '@rel': 'enclosure' }
+          enclosure.type = attachment.mime_type
+          enclosure.href = attachment.url
+          if (attachment.size_in_bytes) enclosure.length = attachment.size_in_bytes
+          entry.link.push(enclosure)
+        })
+      }
+
+      if (item.summary) {
+        entry.summary = item.summary
+      }
+
+      if (item.tags) {
+        entry.category = item.tags.map(tag => ({ '@term': tag }))
+      }
+
+      atom.feed.entry.push(entry)
+    })
+    if (mostRecentlyUpdated > '0') atom.feed.updated = mostRecentlyUpdated
+  }
+
+  return atom
+}
+
+function generateRequiredTitle (item) {
+  if (item.title) return item.title
+  if (item.summary) return truncate(cleanWhitespace(item.summary))
+  if (item.content_text) return truncate(cleanWhitespace(item.content_text))
+  if (item.content_html) return truncate(cleanWhitespace(striptags(item.content_html)))
+  throw new Error('jsonfeed-to-atom: can\'t generate a title for entry ' + item.id)
+}
+
+function cleanWhitespace (summary) {
+  return trimLeft(trimRight(summary.split('\n')[0]))
+}
+
+function truncate (string) {
+  return string.length > 100 ? string.slice(0, 100) + '…' : string
+}
+
+function jsonfeedToAtom (jsonfeed) {
+  const feedObj = jsonfeedToAtomObject(jsonfeed)
+  const feed = builder.create(feedObj, { encoding: 'utf-8' })
+  return feed.end({ pretty: true })
+}
+
+module.exports = jsonfeedToAtom
