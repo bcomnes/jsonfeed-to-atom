2553da77e0c3c792cb6fe63f8e41994d69f799b2
Bret Comnes <bcomnes@gmail.com>
2018-04-15 12:59:46 -0700

    Cover it all!

--- jsonfeed-to-atom-object.js
+++ jsonfeed-to-atom-object.js
@@ -1,7 +1,5 @@
 const packageInfo = require('./package.json')
-const striptags = require('striptags')
-const trimRight = require('trim-right')
-const trimLeft = require('trim-left')
+const generateTitle = require('./lib/generate-title')
 
 /**
  * Converts a JSON feed to an atom feed xmlbuilder object
@@ -57,7 +55,7 @@ module.exports = function jsonfeedToAtomObject (jf) {
     jf.items.forEach(item => {
       const entry = { // Required fields
         id: item.id,
-        title: generateRequiredTitle(item),
+        title: generateTitle(item),
         updated: item.date_modified || item.date_published || now.toISOString()
       }
       if (item.date_published) entry.published = item.date_published
@@ -145,37 +143,4 @@ module.exports = function jsonfeedToAtomObject (jf) {
   }
 
   return atom
-}
-
-/**
- *  Generate the required atom title for a JSON feed item
- * @param {object} item A parsed JSON feed item
- * @return {string} A title for the item
- */
-function generateRequiredTitle (item) {
-  // A fairly arbitrary attempt at generating titles.
-  // Ideally your JSON feed includes a title for every post. Most UI's will look strange without it
-  if (item.title) return item.title
-  if (item.summary) return truncate(cleanWhitespace(item.summary))
-  if (item.content_text) return truncate(cleanWhitespace(item.content_text))
-  if (item.content_html) return truncate(cleanWhitespace(striptags(item.content_html)))
-  throw new Error('jsonfeed-to-atom: can\'t generate a title for entry ' + item.id)
-}
-
-/**
- * Removes title unfriendly whitespace
- * @param {string} str The string you want cleaned up
- * @return {string} The first line of the string without any padding
- */
-function cleanWhitespace (str) {
-  return trimLeft(trimRight(str.split('\n')[0]))
-}
-
-/**
- * Truncate a string to 100 characters with an ellipsis
- * @param {string} string The string you want truncated
- * @returns {string} The truncated string
- */
-function truncate (string) {
-  return string.length > 100 ? string.slice(0, 100) + '…' : string
 }
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const jsonfeedToAtomObject = require('./jsonfeed-to-atom-object')
 const testFeed = require('./test-feed.json')
 const jsonfeedToAtom = require('./')
+const generateTitle = require('./lib/generate-title')
 const test = require('tape')
 const fs = require('fs')
 
@@ -25,6 +26,28 @@ test('missing property errors', t => {
       title: 'A feed title'
     })
   }, /missing feed_url/, 'Missing version throw')
+
+  t.end()
+})
+
+test('generateTitle', t => {
+  const simpleTitle = 'Hey this is a title'
+  t.equal(generateTitle({ title: simpleTitle }), simpleTitle, 'title properties are passed through')
+
+  const missingTitle = {
+    summary: 'This is a summary',
+    content_text: 'This is some text',
+    content_html: '<p>yo</p>  '
+  }
+  t.equal(generateTitle(missingTitle), missingTitle.summary, 'use a summary if present')
+
+  const missingSummary = Object.assign({}, missingTitle, { summary: null })
+  t.equal(generateTitle(missingSummary), missingSummary.content_text, 'use text_content if summary is missing')
+
+  const missingText = Object.assign({}, missingSummary, { content_text: null })
+  t.equal(generateTitle(missingText), 'yo', 'use a cleaned up content_html last')
+
+  t.throws(() => generateTitle({}), /can't generate a title/, 'Will throw if it can\'t generate a title')
 
   t.end()
 })
